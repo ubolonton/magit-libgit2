ea2b547cd56900372c52b3f6b0c390e9aaf65f66
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2018-01-03 21:39:42 +0700

    Use published version of emacs binding

--- bin/load.sh
+++ bin/load.sh
@@ -6,12 +6,17 @@ here=`cd $(dirname $BASH_SOURCE); pwd`
 root=`cd $here/..; pwd`
 RS_MODULE=$(find $root -iname '*emacs_rs_module*.dylib' | head -n 1)
 MODULE=$root/target/debug/libmagit_libgit2.dylib
+MODULE_X=$root/elisp/magit-libgit2-x.el
 
 read -r -d '' expr <<EOF
 (progn
-  (unless (featurep 'rs-module)
+  (unless (featurep 'rs-module ;'
+                              )
     (module-load "$RS_MODULE"))
-  (rs-module/load "$MODULE"))
+  (rs-module/load "$MODULE")
+  (unless (featurep 'magit-libgit2-x ;'
+                                    )
+    (load "$MODULE_X")))
 EOF
 
 emacsclient -e "$expr"
--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,31 @@ extern crate libc;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
-extern crate emacs_module_bindings as emacs;
+extern crate emacs;
 extern crate git2;
 
-use emacs::{EmacsVal, EmacsRT, EmacsEnv};
-use emacs::{Env, HandleFunc};
-use std::os::raw;
 use std::ptr;
+use emacs::EmacsVal;
+use emacs::{Env, HandleFunc, ToEmacs};
 use git2::{Repository};
 
-/// This states that the module is GPL-compliant.
-/// Emacs won't load the module if this symbol is undefined.
-#[no_mangle]
-#[allow(non_upper_case_globals)]
-pub static plugin_is_GPL_compatible: libc::c_int = 0;
+emacs_plugin_is_GPL_compatible!();
+emacs_module_init!(init);
 
 const MODULE: &str = "magit-libgit2";
 lazy_static! {
     static ref MODULE_PREFIX: String = format!("{}/", MODULE);
 }
 
-fn git_rev_parse(env: &Env, args: &[EmacsVal], _data: *mut raw::c_void) -> emacs::Result<EmacsVal> {
+fn git_rev_parse(env: &Env, args: &[EmacsVal], _data: *mut libc::c_void) -> emacs::Result<EmacsVal> {
     let path: String = env.from_emacs(args[0])?;
     let spec: String = env.from_emacs(args[1])?;
     let repo = Repository::discover(&path).map_err(emacs::Error::new)?;
     let obj = repo.revparse_single(&spec).map_err(emacs::Error::new)?;
-    env.to_emacs(obj.id().to_string())
+    obj.id().to_string().to_emacs(env)
 }
 
-fn init(env: &Env) -> emacs::Result<EmacsVal> {
+fn init(env: &mut Env) -> emacs::Result<EmacsVal> {
     macro_rules! prefixed {
         ($name:expr) => {
             &format!("{}{}", *MODULE_PREFIX, $name)
@@ -47,19 +43,4 @@ fn init(env: &Env) -> emacs::Result<EmacsVal> {
     )?;
 
     env.provide(MODULE)
-}
-
-/// Entry point for live-reloading during development.
-#[no_mangle]
-pub extern "C" fn emacs_rs_module_init(raw: *mut EmacsEnv) -> libc::c_int {
-    match init(&Env::from(raw)) {
-        Ok(_) => 0,
-        Err(_) => 1,
-    }
-}
-
-/// Entry point for Emacs's loader, for "production".
-#[no_mangle]
-pub extern "C" fn emacs_module_init(ert: *mut EmacsRT) -> libc::c_int {
-    emacs_rs_module_init(Env::from(ert).raw())
 }
