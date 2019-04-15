15974c765a01feadb2a890c3ddf8bc8bbe24fd3c
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-04-15 20:12:12 +0700

    Update emacs crate to 0.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,17 @@
-extern crate libc;
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate emacs;
-extern crate git2;
+use emacs::{defun, Env, Result};
+use git2::Repository;
 
-use emacs::{Env, CallEnv, Value, Result};
-use git2::{Repository};
+emacs::plugin_is_GPL_compatible!();
 
-emacs_plugin_is_GPL_compatible!();
-emacs_module_init!(init);
-
-const MODULE: &str = "magit-libgit2";
-lazy_static! {
-    static ref MODULE_PREFIX: String = format!("{}/", MODULE);
+#[emacs::module(separator = "/")]
+fn init(_: &Env) -> Result<()> {
+    Ok(())
 }
 
-fn rev_parse(env: &CallEnv) -> Result<String> {
-    let path: String = env.parse_arg(0)?;
-    let spec: String = env.parse_arg(1)?;
+/// Parse the given rev using libgit2.
+#[defun]
+fn rev_parse(path: String, spec: String) -> Result<String> {
     let repo = Repository::discover(&path)?;
     let obj = repo.revparse_single(&spec)?;
     Ok(obj.id().to_string())
-}
-
-fn init(env: &Env) -> Result<Value> {
-    emacs_export_functions! {
-        env, *MODULE_PREFIX, {
-            "rev-parse" => (rev_parse, 2..2, "Parse the given rev using libgit2.")
-        }
-    }
-
-    env.provide(MODULE)
 }
